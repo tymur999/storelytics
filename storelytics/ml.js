399ff0ec9f6f90b7e4c2ca9f4dd240118bdcb550
Jae <parkjae433@gmail.com>
2022-07-27 16:15:22 -0500

    init

---
+++ storelytics/ml.js
@@ -0,0 +1,13 @@
+import * as cv from "opencv"
+
+const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
+
+const getFaceImage = (grayImg) => {
+  const faceRects = classifier.detectMultiScale(grayImg).objects;
+  if (!faceRects.length) {
+    throw new Error('failed to detect faces');
+  }
+  return grayImg.getRegion(faceRects[0]);
+};
+
+
